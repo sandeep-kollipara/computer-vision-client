4010c06be9e4fd203dfba9baeac9280fc4764feb
SandyVii <sandeep.kollipara@icloud.com>
2024-05-02 08:01:28 +0530

    ADded Console Log!

--- motion_detection.py
+++ motion_detection.py
@@ -3,8 +3,12 @@
 import os
 import sys
 import cv2
+import logging
+from datetime import datetime
 
 IP_ADDRESS = 'cam.sandy.plus'
+SCREEN_WIDTH = 640 # Can be automated
+SCREEN_HEIGHT = 480 # Can be automated
 
 
 
@@ -14,10 +18,11 @@ if __name__ == '__main__':
     
     image = video.read()[1]
     cv2.imwrite(r"images/background.png", image)
-    background = cv2.imread(r"images/background_night.png") #CHange this back to 'background.png' for auto-initialization
+    background = cv2.imread(r"images/background_day.png") #CHange this back to 'background.png' for auto-initialization
     background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
     background = cv2.GaussianBlur(background,(21,21),0)
     
+    timer = datetime.now().second + 5 # Giving 5 second headstart before logging warnings
     while True:
         status, frame = video.read()
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -31,11 +36,33 @@ if __name__ == '__main__':
         cnts,res = cv2.findContours(thresh.copy(),
                                 cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
+        i = 0
         for contour in cnts:
             if cv2.contourArea(contour) < 10000 :
                  continue
             (x,y,w,h) = cv2.boundingRect(contour)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0), 3)
+            
+            if (x+w)/2 < SCREEN_WIDTH/3:
+                hori_pos = "Left"
+            elif (x+w)/2 > 2*SCREEN_WIDTH/3:
+                hori_pos = "Right"
+            else:
+                hori_pos = "Center"
+            if (y+h)/2 < SCREEN_HEIGHT/3:
+                vert_pos = "Bottom"
+            elif (y+h)/2 > 2*SCREEN_HEIGHT/3:
+                vert_pos = "Top"
+            else:
+                vert_pos = "Middle"
+            
+            if datetime.now().second == timer:
+                logging.warning("[Time:"+datetime.now().strftime("%H:%M:%S")+"]Object #"+str(i+1)+" detected: "+vert_pos+"-"+hori_pos+" Wdt: "+str(w)+"px Hgt "+str(h)+"px.")
+            if timer > 60:
+                timer = 0
+            else:
+                timer += 1
+            i+=1
             
         cv2.imshow("All Contours",frame)
         
